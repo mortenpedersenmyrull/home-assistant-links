228c291a72fa42d68b4fc08c12fae1b346a4aa75
Morten Pedersen <morten9516@gmail.com>
2022-04-08 20:26:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href='http://10.147.18.236:8123/' ><h1>Home assistant hjemme</h1></a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
