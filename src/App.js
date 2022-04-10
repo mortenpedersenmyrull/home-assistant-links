bf6821d564631304624fde82fa39460254ac41eb
mortenpedersenmyrull <101947501+mortenpedersenmyrull@users.noreply.github.com>
2022-04-11 00:29:35 +0200

    Add Låvan home assistant link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <a href='http://10.147.18.236:8123/' ><h1>Home assistant hjemme</h1></a>
+        <a href='http://10.147.18.12:8123/' ><h1>Home assistant Låvan</h1></a>
       </header>
     </div>
   );
